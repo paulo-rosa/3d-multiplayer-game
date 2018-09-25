9941ff977146a1e0ab7730b8e37a243e8965c79a
Paulo Douglas da Rosa <pdouglasrs@gmail.com>
2018-09-25 20:52:37 -0300

    Initial Commit

---
+++ 3DMultiplayerGame/Assets/Scripts/CarBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour {
+
+    public float velocity = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var direction = Input.GetAxis("Horizontal");
+        transform.Rotate(0, direction, 0);
+
+        var speed = Input.GetAxis("Vertical") * velocity;
+        transform.position +=  transform.forward * speed * Time.deltaTime;
+
+        if (speed < 0)
+        {
+            transform.forward = new Vector3(0, 0, speed);
+        }
+    }
+}
