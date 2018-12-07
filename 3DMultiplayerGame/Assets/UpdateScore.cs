2730e245bda847423ce12bc78ac67d05dea0ab50
DESKTOP-BO8A4SQ\renan <renan1504@gmail.com>
2018-12-06 22:53:24 -0200

    menu

---
+++ 3DMultiplayerGame/Assets/UpdateScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour {
+
+    public Text TxtScore;
+
+    // Use this for initialization
+
+    private void OnEnable()
+    {
+        TxtScore.text = "Final Score: " + GameManager.Instance.GetScore();
+    }
+}
